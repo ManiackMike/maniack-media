4ba69ebc3fe291c282c8c004eda3007c608ac9ad
ManiackMike <105581753+ManiackMike@users.noreply.github.com>
2022-05-28 23:15:57 +0200

    Add files via upload

---
+++ Notizie_files/Notizie.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20220528
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Notizie_files/NotizieMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ iPhone_Network_files/iPhone_Network.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20220528
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('iPhone_Network_files/iPhone_NetworkMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
